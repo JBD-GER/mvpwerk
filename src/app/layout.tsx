0163363de8dbe1dab4f226beaa52867e56ef7e93
JBD-GER <124248884+JBD-GER@users.noreply.github.com>
2025-12-16 11:23:38 +0100

    dasdas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,8 @@ import GoogleTag from '@/app/components/GoogleTag'
  * Damit Seiten wirklich pro Request anhand der SearchParams rendern
  * (und nicht statisch “festgebacken” sind).
  */
-
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
 
 const inter = Inter({
   subsets: ['latin'],
